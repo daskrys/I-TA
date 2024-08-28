a8e505f2c6860f7ab0446145a1d9da86ab8100ad
daskrys <caperezl@ucsc.edu>
2024-08-28 16:12:58 -0700

    add common ports and csv export

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import csv
 
 parsed_logs = []
+lookup_table = {}
+lookup_count = {}
 FILE_PATH = "logdata.txt"
 
 IANA_PROTOCOLS = {
@@ -17,7 +19,8 @@ IANA_PROTOCOLS = {
     "41": "IPv6",
 }
 
-EMAIL = [109, 110, 143, 158, 209, 993]
+EMAIL_PORTS = [109, 143, 158, 209, 993]
+COMMON_PORTS = [0, 20, 21, 22, 23, 25, 53, 80, 110, 119, 123, 161, 194, 443]
 
 def parse_line(list):
     return {
@@ -46,12 +49,71 @@ def read_data():
                 parse = parse_line(list)
                 parsed_logs.append(parse)
 
+def assign_tag(port, protocol):
+
+    if port >= 49152:
+        return "private"
+    elif port in EMAIL_PORTS:
+        return "email"
+    elif port == 3389:
+        return "sv_P5"
+    elif (protocol == "UDP") or (protocol == "TCP"):
+        if port in COMMON_PORTS:
+            return "sv_P1"
+        else:
+            return "sv_P2"
+    elif port in COMMON_PORTS:
+        return "sv_P3"
+    else:
+        return "sv_P4"
+
+def export_to_csv():
+
+    tags = {}
+
+    with open("logs.csv", 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        csvwriter.writerow(['dstport', 'protocol', 'tag'])
+
+        for temp in lookup_table:
+            port, protocol = temp
+            tag, count = lookup_table[temp]
+
+            if tag in tags:
+                tags[tag] += 1
+            else:
+                tags[tag] = 1
+
+            csvwriter.writerow([port, protocol, tag])
+
+        csvwriter.writerow(['Tag', 'Count'])
+
+        for t in tags:
+            csvwriter.writerow([t, tags[t]])
+
+        csvwriter.writerow(['Port', 'Protocol', 'Count'])
+
+        for temp in lookup_table:
+            port, protocol = temp
+            tag, count = lookup_table[(port, protocol)]
+            csvwriter.writerow([port, protocol, count])
+        
+
 if __name__ == "__main__":
     read_data()
-    
+
     for log in parsed_logs:
         dstport = log["dstport"]
         protocol_num = log["protocol"]
         protocol = IANA_PROTOCOLS[protocol_num]
 
-        print(f"{dstport} and {protocol}")
+        tag = assign_tag(dstport, protocol)
+        
+        if (dstport, protocol) not in lookup_table:
+            lookup_table[(dstport, protocol)] = tag, 1
+        else:
+            temp, count = lookup_table[(dstport, protocol)]
+            lookup_table[(dstport, protocol)] = tag, count + 1
+
+    export_to_csv()
